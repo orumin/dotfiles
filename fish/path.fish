1f7e8cbcb095d5830da96ddba2e66460f691e893
orumin <dev@orum.in>
2019-02-20 14:52:07 +0900

    bump

--- fish/path.fish
+++ fish/path.fish
@@ -1,28 +1,28 @@
 set ostype (uname)
-switch $ostype
-    case Darwin
-        set -x PATH $PATH "/Library/TeX/texbin"
-        set -x PATH $PATH "/Applications/Adobe Acrobat DC/Adobe Acrobat.app/Contents/MacOS/"
-        set -x PATH $PATH "/Applications/Skim.app/Contents/MacOS/"
-        set -x PATH $PATH "/Applications/Firefox.app/Contents/MacOS/"
-        set -x PATH $PATH "/Applications/Google Chrome.app/Contents/MacOS/"
-        set -x PATH $PATH "/Applications/Vivaldi.app/Contents/MacOS/"
-        set -x PATH $PATH "/Applications/VirtualBox.app/Contents/MacOS/"
-        set -x PATH $PATH "/Applications/Wine Staging.app/Contents/Resources/wine/bin/"
-        set -x PATH "/usr/local/opt/inetutils/libexec/gnubin" $PATH
-        set -x MANPATH "/usr/local/opt/inetutils/libexec/gnuman" $MANPATH
-    end
+
+if test $ostype=Darwin
+    set -x PATH $PATH "/Library/TeX/texbin"
+    set -x PATH $PATH "/Applications/Adobe Acrobat DC/Adobe Acrobat.app/Contents/MacOS/"
+    set -x PATH $PATH "/Applications/Skim.app/Contents/MacOS/"
+    set -x PATH $PATH "/Applications/Firefox.app/Contents/MacOS/"
+    set -x PATH $PATH "/Applications/Google Chrome.app/Contents/MacOS/"
+    set -x PATH $PATH "/Applications/Vivaldi.app/Contents/MacOS/"
+    set -x PATH $PATH "/Applications/VirtualBox.app/Contents/MacOS/"
+    set -x PATH $PATH "/Applications/Wine Staging.app/Contents/Resources/wine/bin/"
+    set -x PATH "/usr/local/opt/inetutils/libexec/gnubin" $PATH
+    set -x MANPATH "/usr/local/opt/inetutils/libexec/gnuman" $MANPATH
+end
 
 set -x PATH $PATH "$PSPDEV/bin" "$HOME/bin"
 set -x PATH $PATH "$VITASDK/bin"
 
-if type -q ruby
+if type -q -f ruby
     set -x PATH (ruby -rrubygems -e "puts Gem.user_dir")/bin $PATH
 end
 
-set -x PATH "/home/orumin/.cabal/bin" $PATH
-set -x PATH "/usr/lib/ccache/bin" $PATH
-set -x PATH "/usr/share/git/diff-highlight" $PATH
+set -x PATH "/home/orumin/.cabal/bin" $PATH ^/dev/null
+set -x PATH "/usr/lib/ccache/bin" $PATH ^/dev/null
+set -x PATH "/usr/share/git/diff-highlight" $PATH ^/dev/null
 
 #set -x PATH "$HOME/.cargo/bin" $PATH
 #set -x PATH "/usr/lib/smlnj/bin" $PATH
@@ -36,6 +36,6 @@ set -x MANPATH "/usr/share/man/ja" "/usr/share/man" "/usr/local/man" "/usr/local
 set -x MANPATH $MANPATH "/usr/i486-mingw32/share/man" "/opt/qt/man"
 set -x MANPATH $MANPATH "/opt/pspsdk/man" "/opt/pspsdk/psp/man" "/opt/pspsdk/psp/share/man"
 
-if type -q rbenv
+if type -q -f rbenv
     rbenv init - | source
 end
