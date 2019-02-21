9e57d29bd11704be5279a42ee8d03c59f3b701b8
orumin <dev@orum.in>
2019-02-22 05:55:06 +0900

    bump

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,20 @@
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
 function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
     set -l bg_color black
@@ -22,7 +39,7 @@ function fish_prompt --description 'Write out the prompt'
     _append left_prompt (echo_color -b $bg_color $fg_color "<$prompttty>")
 
     # VCS
-    set -l git (__terlar_git_prompt)
+    set -l git (__fish_git_prompt)
     if test -n "$git"
         _append left_prompt (echo_color -b $bg_color "$git")
     end
