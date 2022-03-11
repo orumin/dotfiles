52673f4e60252b2a56cc3495b45ccab82c3ddc9b
orumin <dev@orum.in>
2022-03-11 17:00:17 +0900

    use new option for lspkind.nvim

--- .vim/rc/plugins/lua/plugin-lspkind.lua
+++ .vim/rc/plugins/lua/plugin-lspkind.lua
@@ -1,8 +1,14 @@
 require('lspkind').init({
+    -- DEPRECATED (use mode instead): enables text annotations -- DEPRECATED (use mode instead): enables text annotations
     -- enables text annotations
     --
     -- default: true
-    with_text = true,
+    -- with_text = true,
+
+    -- defines how annotations are shown
+    -- default: symbol
+    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+    mode = 'symbol_text',
 
     -- default symbol map
     -- can be either 'default' (requires nerd-fonts font) or
