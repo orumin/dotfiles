41b27205eb00927012ad0b82971f7e00a6bc9895
orumin <dev@orum.in>
2023-06-13 20:17:54 +0900

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,12 +8,29 @@ end
 
 vim.o.shell = "bash"
 
-require("lib.utils")
+local utils = require("lib")
+utils.disable_rtp_plugins()
 
 require('plugins')
 require('core.autocmd')
 require('core.basic')
-require('core.color')
 require('core.diagnostic')
 require('core.encoding')
 require('core.keymaps')
+
+local color
+if vim.env.COLORTERM and
+  vim.env.COLORTERM == "truecolor" then
+  vim.o.termguicolors = true
+  if gui_running then
+    vim.o.guicolors = true
+  end
+  color = "catppuccin"
+
+else
+  vim.o.t_Co = "256"
+  color = "monokai"
+end
+
+vim.cmd.colorscheme(color)
+
---
+++ nvim/lua/lib/init.lua
@@ -0,0 +1,88 @@
+local M = {}
+
+function M.setting_global()
+  _G.pr_error = function (msg, opts)
+    vim.notify(msg, vim.log.levels.ERROR, opts)
+  end
+
+  _G.nnoremap = function (lhs, rhs)
+    vim.keymap.set("n", lhs, rhs, {noremap = true, silent = true})
+  end
+
+  local uv
+  if vim.uv then uv = vim.uv else uv = vim.loop end
+
+  local uname = uv.os_uname()
+
+  _G.gui_running = vim.fn.has("gui_running") == 1
+  _G.is_win = uname.sysname == "Windows"
+  _G.is_mac = uname.sysname == "Darwin"
+  _G.is_linux = uname.sysname == "Linux"
+  _G.is_wsl = is_linux and string.find(uname.release, "microsoft") ~= nil
+  _G.path_sep = is_win and "\\" or "/"
+
+  if vim.env.XDG_CACHE_HOME then
+    _G.nvim_cache_dir = vim.env.XDG_CACHE_HOME .. path_sep .. 'nvim'
+  else
+    _G.nvim_cache_dir = uv.os_homedir() .. path_sep .. '.cache' .. path_sep .. 'nvim'
+  end
+
+  _G.homedir = uv.os_homedir()
+end
+
+
+function M.disable_rtp_plugins ()
+  --disable menu loading
+  vim.g.did_install_default_menus = 1
+  vim.g.did_install_syntax_menu   = 1
+
+  vim.g.did_indent_on             = 1
+
+  -- disable load after/ftplugin
+  --vim.g.did_load_filetypes        = 1
+  --vim.g.did_load_ftplugin         = 1
+
+  -- don't load native syntax completion
+  vim.g.loaded_syntax_completion  = 1
+  -- don't load sql omni completion
+  vim.g.loaded_sql_completion  = 1
+  -- don't load spell files
+  vim.g.loaded_spellfile_plugin   = 1
+
+  --vim.g.loaded_man                = 1
+
+  -- disable load tutor plugin
+  vim.g.loaded_tutor_mode_plugin  = 1
+
+  -- don't load tohtml.vim
+  vim.g.loaded_2html_plugin       = 1
+
+  -- netrw liststyle: https://medium.com/usevim/the-netrw-style-options-3ebe91d42456
+  --vim.g.loaded_netrwPlugin        = 1
+  vim.g.netrw_liststyle           = 3
+
+  -- don't use built-in matchit.vim and matchiparen.vim
+  -- since the use of vim-matchup
+  --vim.g.loaded_matchit            = 1
+  --vim.g.loaded_matchparen         = 1
+
+  -- don't load plugin for archive format file
+  -- (related to checking files inside archive)
+  vim.g.loaded_gzip               = 1
+  vim.g.loaded_tar                = 1
+  vim.g.loaded_tarPlugin          = 1
+  vim.g.loaded_vimball            = 1
+  vim.g.loaded_vimballPlugin      = 1
+  vim.g.loaded_zip                = 1
+  vim.g.loaded_zipPlugin          = 1
+
+  vim.g.skip_loading_mswin        = 1
+
+  vim.g.loaded_shada_plugin       = 1
+  -- disable remote plugins
+  vim.g.loaded_remote_plugins     = 1
+end
+
+M.setting_global()
+
+return M
