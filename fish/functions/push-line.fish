c8868ebdc0ff85a35bc8493b51c9a0108ecb75f3
orumin <dev@orum.in>
2024-08-01 16:19:15 +0900

    fish: update push-line.fish

--- fish/functions/push-line.fish
+++ fish/functions/push-line.fish
@@ -9,11 +9,10 @@
 function push-line
     set -g __fish_pushed_line (commandline)
     commandline ""
-    function on-next-prompt --on-event fish_prompt
+    function after-next-prompt --on-event fish_postexec
         commandline $__fish_pushed_line
-        functions --erase on-next-prompt
+        functions --erase after-next-prompt
     end
 end
 
-# https://github.com/fish-shell/fish-shell/issues/6973
-# https://gist.github.com/turanegaku/9ac0a75ea08dd353b5d8d0f21149666d#file-push-line-fish
+# https://github.com/fish-shell/fish-shell/issues/6973#issuecomment-1379827312
