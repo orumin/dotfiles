e0af7ce7e8bfc7148f958a49ba3a8ea4895fe839
orumin <dev@orum.in>
2025-04-16 21:43:25 +0900

    bat: update configuration

--- fish/environment.fish
+++ fish/environment.fish
@@ -9,6 +9,10 @@ else
     set -x EDITOR vi
 end
 
+if type -q bat
+    set -x MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+end
+
 if set -q COLORTERM
     set -x COLORTERM "$COLORTERM"
 end
