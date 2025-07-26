fab92fa9970f181daf5e4a02b6ad7139214f6219
orumin <dev@orum.in>
2025-07-27 04:36:41 +0900

    fix(nvim): remove unnecessary plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufReadPost" },
     dependencies = {
-      { "cathaysia/tree-sitter-loongdoc" },
       { "nvim-treesitter/nvim-treesitter-context" },
       {
         "NvChad/nvim-colorizer.lua",
