b2f2d120abefb19f37453fbecc185fd4ef62e72a
orumin <dev@orum.in>
2025-10-15 19:42:38 +0900

    feat!(hammerspoon): add dropdown animation

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,12 +1,15 @@
-local function wez_resize(wez)
+---@param wez userdata
+---@param height_ratio number
+local function wez_resize(wez, height_ratio)
   if not wez then return end
+  if not height_ratio then return end
   local window = wez:focusedWindow()
   local f = window:frame()
   local max = window:screen():frame()
   f.x = max.x
   f.y = max.y
   f.w = max.w
-  f.h = max.h * 0.7
+  f.h = max.h * height_ratio
   window:setFrame(f)
 end
 
@@ -15,13 +18,14 @@ hs.hotkey.bind({"ctrl"}, ";", function()
   if not wez then
     hs.application.launchOrFocus(os.getenv("HOME") .. "/working/wezterm/target/release/wezterm-gui")
     wez = hs.application.get("com.github.wez.wezterm")
-    wez_resize(wez)
+    wez_resize(wez, 0.7)
   else
     if wez:isFrontmost() then
       wez:hide()
+      wez_resize(wez, 0.0)
     else
       wez:activate()
-      wez_resize(wez)
+      wez_resize(wez, 0.7)
     end
   end
 end)
